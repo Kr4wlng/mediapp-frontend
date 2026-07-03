4805bd24be128ae25a84fa6ff01ca91c07b16b59
adurand <adurand@sintad.pe>
2026-07-03 10:49:34 -0500

    Angular MatTable

--- src/app/pages/patient/patient.component.ts
+++ src/app/pages/patient/patient.component.ts
@@ -1,21 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { PatientService } from '../../services/patient.service';
 import { Patient } from '../../model/patient';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-patient',
-  imports: [],
+  imports: [MatTableModule, MatButtonModule, MatIconModule],
   templateUrl: './patient.component.html',
   styleUrl: './patient.component.css',
 })
 export class PatientComponent implements OnInit{
 
-  patients: Patient[] = [];
+  // patients: Patient[] = [];
+  dataSource = new MatTableDataSource<Patient>([]);
+  // displayedColumns: string[] = ['idPatient', 'firstName', 'lastName', 'dni'];
+  columnsDefinitions = [
+    { def: 'idPatient', label: 'idPatient', hide: true },
+    { def: 'firstName', label: 'firstName', hide: false },
+    { def: 'lastName', label: 'idPatilastNameent', hide: false },
+    { def: 'dni', label: 'dni', hide: false },
+    { def: 'actions', label: 'actions', hide: false }
+  ]
 
   constructor(private patientService: PatientService){}
 
   ngOnInit(): void {
-    this.patientService.findAll().subscribe(data => this.patients = data);
+    this.patientService.findAll().subscribe(data => {
+      this.dataSource.data = data;
+    });
+  }
+
+  getDisplayColumns(){
+    return this.columnsDefinitions.filter(cd => !cd.hide).map(cd => cd.def);
   }
 
 }
