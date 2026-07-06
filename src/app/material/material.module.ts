34ea73af461f733609b7667e33188da4a678692a
adurand <adurand@sintad.pe>
2026-07-06 10:50:05 -0500

    Angular MatSort y MatPaginator

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -7,6 +7,10 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 
 @NgModule({
@@ -21,7 +25,11 @@ import { MatDividerModule } from '@angular/material/divider';
     MatSidenavModule,
     MatToolbarModule,
     MatMenuModule,
-    MatDividerModule
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSortModule,
+    MatPaginatorModule
   ]
 })
 export class MaterialModule { }
--- src/app/pages/patient/patient.component.ts
+++ src/app/pages/patient/patient.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { PatientService } from '../../services/patient.service';
 import { Patient } from '../../model/patient';
 import {MatTableDataSource} from '@angular/material/table';
 import { MaterialModule } from '../../material/material.module';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-patient',
@@ -23,16 +25,25 @@ export class PatientComponent implements OnInit{
     { def: 'actions', label: 'actions', hide: false }
   ]
 
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   constructor(private patientService: PatientService){}
 
   ngOnInit(): void {
     this.patientService.findAll().subscribe(data => {
       this.dataSource.data = data;
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
     });
   }
 
   getDisplayColumns(){
     return this.columnsDefinitions.filter(cd => !cd.hide).map(cd => cd.def);
+  }
+
+  applyFilter(e: any){
+    this.dataSource.filter = e.target.value.trim();
   }
 
 }
