031913264c4accb03050d36d4d2596b178958359
adurand <adurand@sintad.pe>
2026-07-06 23:37:07 -0500

    Validacion de formularios

--- src/app/pages/patient/patient-edit/patient-edit.component.ts
+++ src/app/pages/patient/patient-edit/patient-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MaterialModule } from '../../../material/material.module';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from "@angular/router";
 import { PatientService } from '../../../services/patient.service';
 import { Patient } from '../../../model/patient';
@@ -27,12 +27,12 @@ export class PatientEditComponent implements OnInit{
   ngOnInit(): void {
     this.form = new FormGroup({
       idPatient: new FormControl(0),
-      firstName: new FormControl(''),
-      lastName: new FormControl(''),
-      dni: new FormControl(''),
-      address: new FormControl(''),
-      phone: new FormControl(''),
-      email: new FormControl(''),
+      firstName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(70)]),
+      lastName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(70)]),
+      dni: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.pattern('[0-9]+')),
+      email: new FormControl('', Validators.email),
     });
 
     this.route.params.subscribe(data => {
@@ -48,12 +48,12 @@ export class PatientEditComponent implements OnInit{
       this.patientService.findById(this.id).subscribe(data => {
         this.form = new FormGroup({
           idPatient: new FormControl(data.idPatient),
-          firstName: new FormControl(data.firstName),
-          lastName: new FormControl(data.lastName),
-          dni: new FormControl(data.dni),
-          address: new FormControl(data.address),
-          phone: new FormControl(data.phone),
-          email: new FormControl(data.email),
+          firstName: new FormControl(data.firstName, [Validators.required, Validators.minLength(3), Validators.maxLength(70)]),
+          lastName: new FormControl(data.lastName, [Validators.required, Validators.minLength(3), Validators.maxLength(70)]),
+          dni: new FormControl(data.dni, Validators.required),
+          address: new FormControl(data.address, Validators.required),
+          phone: new FormControl(data.phone, Validators.pattern('[0-9]+')),
+          email: new FormControl(data.email, Validators.email),
         });
       })
     }
@@ -93,6 +93,15 @@ export class PatientEditComponent implements OnInit{
     }
 
     this.router.navigate(['pages/patient']);
+  }
+
+  get f(){
+    return this.form.controls;
+  }
+
+  checkFirstName(): boolean{
+    let firstName: string = this.form.value['firstName'];
+    return firstName.length === 0;
   }
 
 }
