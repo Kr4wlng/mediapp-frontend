00296dae6f5ac4bee4b467d373cdb193effc351c
adurand <adurand@sintad.pe>
2026-07-09 17:22:46 -0500

    HttpInterceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,18 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { ServerErrorInterceptor } from './interceptor/server-error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient() // Con esto activamos las peticiones Http
+    provideHttpClient(withInterceptorsFromDi()), // Con esto activamos las peticiones Http
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ServerErrorInterceptor,
+      multi: true
+    }
   ]
 };
